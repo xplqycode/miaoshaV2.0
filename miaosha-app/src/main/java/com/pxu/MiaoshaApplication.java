d0f69abb4a6f5347a8aed1502aece88f95d007d3
xupeng3 <xupeng3@crop.netease.com>
2020-07-05 21:33:51 +0800

    update .gitignore

---
+++ miaosha-app/src/main/java/com/pxu/MiaoshaApplication.java
@@ -0,0 +1,15 @@
+package com.pxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xupeng3@corp.netease.com
+ * @date 2020/7/5 21:00
+ */
+@SpringBootApplication
+public class MiaoshaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoshaApplication.class, args);
+    }
+}
