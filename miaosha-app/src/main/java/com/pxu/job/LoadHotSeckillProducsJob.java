f9e3db1fb5e0d82bf47035a23c77a7830dde0a9a
xupeng3 <pxu31@qq.com>
2020-07-27 21:01:30 +0800

    缓存预热，防止雪崩，待开发

--- miaosha-app/src/main/java/com/pxu/job/LoadHotSeckillProducsJob.java
+++ miaosha-app/src/main/java/com/pxu/job/LoadHotSeckillProducsJob.java
@@ -13,5 +13,6 @@ public class LoadHotSeckillProducsJob implements SimpleJob {
     @Override
     public void execute(ShardingContext shardingContext) {
         //执行定时加载hot商品信息到缓存中
+        //缓存预热，固定每小时遍历一次，将秒杀开始时间在五分钟之内的数据放到缓存中
     }
 }
