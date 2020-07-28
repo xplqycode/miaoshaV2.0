a2d1949dfa9e8852a8bb811d666af4dcad64f418
xupeng3 <pxu31@qq.com>
2020-07-28 22:46:30 +0800

    开发缓存预热

--- miaosha-app/src/main/java/com/pxu/job/LoadHotSeckillProducsJob.java
+++ miaosha-app/src/main/java/com/pxu/job/LoadHotSeckillProducsJob.java
@@ -3,6 +3,7 @@ package com.pxu.job;
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author pxu31@qq.com
@@ -10,9 +11,12 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class LoadHotSeckillProducsJob implements SimpleJob {
+
+    @Autowired
+
+
     @Override
     public void execute(ShardingContext shardingContext) {
-        //执行定时加载hot商品信息到缓存中
-        //缓存预热，固定每小时遍历一次，将秒杀开始时间在五分钟之内的数据放到缓存中
+
     }
 }
